8302901e388909a8ff731d09948d422e637d5f58
JDJG <ghostzap123@gmail.com>
2025-03-23 23:43:07 -0400

    Add files via upload

---
+++ list_biggest_processes.py
@@ -0,0 +1,21 @@
+import os
+
+import psutil
+
+processes = psutil.process_iter()
+
+current_process = psutil.Process(os.getpid())
+username = current_process.username()
+
+filtered_processes = list(filter(lambda process: process.username() == username, processes))
+
+sorted_processes = sorted(filtered_processes, key=lambda process: process.memory_percent(), reverse=True)
+
+for process in sorted_processes[:10]:
+        if process.pid != current_process.pid and process.name():
+                try:
+                        print(process.name(), process.cmdline(), process.cwd(), process.username(), f"Memory Percent: {process.memory_percent()}%")
+
+                except:
+                        pass
+                        # nothing here
